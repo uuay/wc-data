0fc162103c3d6bc598339267ddd3e4ee02b2803c
parkhyunkyu <parkhyun52@gmail.com>
2019-04-13 19:59:06 +0900

    feature/F-#25

--- welfare/spiders/welfarespider.py
+++ welfare/spiders/welfarespider.py
@@ -11,7 +11,7 @@ class welfarespider(scrapy.Spider):
     start_urls = ["http://bokjiro.go.kr/nwel/helpus/welsha/selectWelShaInfoBbrdMngList.do"]
 
     def start_requests(self):
-        for i in range(1, 43, 1):
+        for i in range(1, 4, 1):
             yield scrapy.Request(
                 "http://bokjiro.go.kr/nwel/helpus/welsha/selectWelShaInfoBbrdMngList.do?searchCondition=&searchKeyword=&srchDuration=&stDate=&endDate=&pageUnit=10&endSvrEsc=0&intClIdStr=&orderCol=MODDATE&orderBy=DESC&recordCountPerPage=10&viewEndService=&pageIndex=%d" %i, 
                 self.parse)
@@ -22,30 +22,58 @@ class welfarespider(scrapy.Spider):
                 "http://bokjiro.go.kr/" + sel.xpath('td[@class="txtL"]/a/@href').extract()[0], 
                 callback=self.parse_bokjiro)
             yield request
-
+    # name이 a태그일 경우의 xPath                    //*[@id="contents"]/div[2]/table/tbody/tr[2]/td/a[1]
+    # name이 테이블의 td 태그일 경우의 xPath           //*[@id="contents"]/div[2]/table/tbody/tr[2]/td
     def parse_bokjiro(self, response):
         item = WelfareItem()
         service = response.xpath('//section[@id="contents"]/div[2]/table/tbody/tr[1]/td/strong/text()')
         if service:
             item['service'] = service.extract()
-        purpose = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[1]/div/text()')
-        if purpose:
+
+        name = response.xpath('//*[@id="contents"]/div[2]/table/tbody/tr[2]/td/a[1]/text()')
+        if name:
+            item['name'] = name.extract()
+        else:
+            item['name'] = response.xpath('//*[@id="contents"]/div[2]/table/tbody/tr[2]/td/text()').extract()
+     
+        val1 = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[1]/strong/text()')
+        val2 = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[1]/div/text()')
+        if val1 == '사업목적':
+            purpose = val2
             item['purpose'] = purpose.extract()
-        target = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[2]/div/text()')
-        if target:
+        elif val1 == '지원대상':
+            target = val2
             item['target'] = target.extract()
-        content = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[3]/div/text()')
-        if content:
+        elif val1 == '지원내용':
+            content = val2
             item['content'] = content.extract()
-        howto = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[4]/div/text()')
-        if howto:
+        elif val1 == '신청방법':
+            howto = val2
             item['howto'] = howto.extract()
-        docs = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[5]/div/text()')
-        if docs:
+        elif val1 == '제출서류':
+            docs = val2
             item['docs'] = docs.extract()
-        etc = response.xpath('//section[@id="contents"]/div[2]/div/ul/li[6]/div/text()')
-        if etc:
+        elif val1 == '기타':
+            etc = val2
             item['etc'] = etc.extract()
+
+        # if purpose:
+        #     item['purpose'] = purpose.extract()
+
+        # if target:
+        #     item['target'] = target.extract()
+
+        # if content:
+        #     item['content'] = content.extract()
+
+        # if howto:
+        #     item['howto'] = howto.extract()
+
+        # if docs:
+        #     item['docs'] = docs.extract()
+
+        # if etc:
+        #     item['etc'] = etc.extract()
         # if response.xpath('//section[@id="contents"]/div[2]/table/tbody/tr[1]/td/strong/text()').extract()[0] is not None:
         #     item['service'] = response.xpath('//section[@id="contents"]/div[2]/table/tbody/tr[1]/td/strong/text()').extract()[0]
         # if response.xpath('//section[@id="contents"]/div[2]/div/ul/li[1]/div/text()').extract()[0] is not None:
