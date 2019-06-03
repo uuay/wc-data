570d8474c1285e492b86d518182c3e3247f9bdf3
박현규 <parkhyun52@gmail.com>
2019-06-03 22:04:33 +0900

    [#46] 민간 복지정보 수집 - priServices.csv에 all.csv와 같은 형식으로 저장 - selenium 활용 모바일 복지로 웹 크롤링

--- services/src/priServicesCrawl.py
+++ services/src/priServicesCrawl.py
@@ -16,39 +16,56 @@ driver.get(url)
 driver.find_element_by_id("topWSI").click()
 num = driver.find_element_by_id("search_length")
 repeatVal = int(num.text)/5
-print(repeatVal)
-
+count = 0
+f = open('priServices.csv', 'w')
 for i in range(repeatVal) :
-    f = open('priServices.csv', 'w')
     for j in range(5) :
-        innerURL = "//div[@class='service_group_list']/ul[1]/li[" + str(j+1) + "]"
+        count = count + 1
+        for l in range(i) :
+            driver.find_element_by_class_name("ui-button-list-more").click()
+            print("list more button run")
+        innerURL = "//div[@class='service_group_list']/ul[" + str(i+1) + "]/li[" + str(j+1) + "]"
         driver.find_element_by_xpath(innerURL).click()
+        #print(innerURL)
+        time.sleep(2)
         csvRow = ["", "", "", "", "", "", "", "", ""]
-        for k in range(5) :
-            result = driver.find_element_by_xpath("//div[@class='shareServiceCont']/ul/li[" + str(k+1) + "]/strong").text
-            if result.encode('utf-8') == '사업목적' :
-                servDgst = driver.find_element_by_xpath("//div[@class='shareServiceCont']/ul/li[" + str(k+1) + "]/div").text
-                csvRow[4] = servDgst.encode('utf-8')
+        aasc = driver.find_element_by_css_selector("#frm > div.grid-inner-centered > div > div.shareServiceCont > ul > li:last-child")
 
+        parentul = driver.find_element_by_css_selector("#frm > div.grid-inner-centered > div > div.shareServiceCont > ul")
+        liLength = parentul.find_elements_by_css_selector("li")
+        liVal = len(liLength)
+
+        for k in range(liVal) :
+            path = "//div[@class='shareServiceCont']/ul/li[" + str(k+1) + "]"
+            result = driver.find_element_by_xpath(path + "/strong").text
+            if result.encode('utf-8') == '사업목적' :
+                servDgst = driver.find_element_by_xpath(path + "/div").text
+                csvRow[4] = servDgst.encode('utf-8')
+            print("")
         jurMnofNm = driver.find_element_by_xpath("//form[@id='frm']/div[1]/div/table/tbody/tr[1]/td").text
         csvRow[2] = jurMnofNm.encode('utf-8')
+
         servDtlLink = driver.current_url
         csvRow[5] = servDtlLink
+
         servId = servDtlLink.split("&welInfSno=")[1]
         servId = servId.split("&")[0]
         csvRow[6] = servId
+
         servNm = driver.find_element_by_xpath("//h2[@class='title']").text
         csvRow[7] = servNm.encode('utf-8')
+
         svcfrstRegTs = driver.find_element_by_xpath("//form[@id='frm']/div[1]/div/table/tbody/tr[3]/td").text
         csvRow[8] = svcfrstRegTs.encode('utf-8')
 
         wr = csv.writer(f)
         wr.writerow(csvRow)
 
-        time.sleep(2)
         driver.back()
+        time.sleep(2)
         driver.find_element_by_id("topWSI").click()
-    driver.find_element_by_class_name("ui-button-list-more").click()
+
     time.sleep(2)
-    f.close()
+
+f.close()
 driver.quit()
